aa5fc7d00541f9a177bd0f4d734b27a479aa5017
gsingh84 <gsingh84@mail.greenriver.edu>
2018-01-11 17:27:25 -0500

    added view file

--- index.php
+++ index.php
@@ -7,10 +7,9 @@ $f3 = Base::instance();
 
 //Define a default route
 $f3->route('GET /', function() {
-    echo "Pet Home";
-//    $view = new View;
-//    echo $view->render
-//    ('views/fat-free.html');
+    $view = new View;
+    echo $view->render
+    ('views/home.html');
 });
 
 //Run fat free
